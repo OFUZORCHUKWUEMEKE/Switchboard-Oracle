90be2f35f930441eaf8f97d60517e6e4d11395e9
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-10-16 07:17:59 +0000

    seup

---
+++ programs/burry-escrow/src/lib.rs
@@ -0,0 +1,25 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("8vGMbXJUiadkx8BcirGQ2UN1w4e6xb7i3cF4nWzMgLU1");
+
+#[program]
+pub mod burry_escrow {
+    use super::*;
+
+    pub fn deposit(ctx:Context<Deposit>,escrow_amount:u64,unlock_price:f64)->Result<()>{
+        Ok(())
+    }
+
+    pub fn withdraw(ctx:Context<Withdraw>)->Result<()>{
+        Ok(())
+    }
+}
