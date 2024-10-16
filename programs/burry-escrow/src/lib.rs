a68707218e454a1cb1e92c72e900f18c330389b9
OFUZOR CHUKWUEMEKE <uniben2018@gmail.com>
2024-10-16 10:22:12 +0000

    added successfully

--- programs/burry-escrow/src/lib.rs
+++ programs/burry-escrow/src/lib.rs
@@ -15,11 +15,11 @@ declare_id!("8vGMbXJUiadkx8BcirGQ2UN1w4e6xb7i3cF4nWzMgLU1");
 pub mod burry_escrow {
     use super::*;
 
-    pub fn deposit(ctx:Context<Deposit>,escrow_amount:u64,unlock_price:f64)->Result<()>{
-        Ok(())
+    pub fn deposit(ctx: Context<Deposit>, escrow_amount: u64, unlock_price: f64) -> Result<()> {
+        deposit_handler(ctx, escrow_amount, unlock_price)
     }
 
-    pub fn withdraw(ctx:Context<Withdraw>)->Result<()>{
-        Ok(())
+    pub fn withdraw(ctx: Context<Withdraw>) -> Result<()> {
+        withdraw_handler(ctx)
     }
 }
